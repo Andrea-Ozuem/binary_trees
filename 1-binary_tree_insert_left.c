3c24a16d0c1522d2ed30a835d3582f8dd3beeda4
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-02-28 11:01:59 +0100

    Add nill case

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = binary_tree_node(parent, value);
+	if (node == NULL)
+		return (NULL);
 	if (parent->left)
 	{
 		node->left = parent->left;
