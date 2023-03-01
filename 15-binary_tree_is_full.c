a67e1f473ae6ea8ed151a1b02edc917f0e654ef1
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-01 21:15:45 +0100

    task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+  *binary_tree_is_full - checks if a binary tree is full
+  *@tree: tree to be checked
+  *Return: 0 as false otherwise true
+  */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int full = 0;
+/*	int full, left, right;*/
+
+/*	left = right = full = 0;*/
+
+	if (!tree)
+		return (0);
+	if ((tree->left && tree->right) || (tree->left == NULL && tree->right == NULL))
+	{
+		full = 1;
+		if (tree->left)
+		{
+			full = binary_tree_is_full(tree->left);
+				if (!full)
+					return (full);
+		}
+		if (tree->right)
+			full = binary_tree_is_full(tree->right);
+	}
+	return (full);
+}
