b19f1fc9d84985eb04f6c218f954088142f0b281
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-01 21:30:11 +0100

    Betty compliace

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,7 +14,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if ((tree->left && tree->right) || (tree->left == NULL && tree->right == NULL))
+	if ((tree->left && tree->right) || (!tree->left && !tree->right))
 	{
 		full = 1;
 		if (tree->left)
