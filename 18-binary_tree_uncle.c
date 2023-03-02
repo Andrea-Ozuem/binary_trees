2a1f7f0fa73640190ecefabf0b561b72033e1884
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-02 01:20:44 +0100

    Task complete

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,20 +1,22 @@
 #include "binary_trees.h"
 
 /**
-  *binary_tree_uncle - finds the uncle of a node
-  *@node: pointer to the node to find the uncle
-  *Return: pointer to the uncle node
-  */
+ * binary_tree_uncle - find the uncle of a node
+ * @node: node
+ * Return: pointer to the uncle node
+ */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = NULL;
+	binary_tree_t *grandp = NULL;
 
-	parent = node->parent;
-	if (!node || !parent)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (parent->left == node)
-		return (parent->right);
-	if (parent->right == node)
-		return (parent->left);
-	return (NULL);
+	grandp = node->parent->parent;
+	if (grandp == NULL)
+		return (NULL);
+
+	if (grandp->left == node->parent)
+		return (grandp->right);
+
+	return (grandp->left);
 }
